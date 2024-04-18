c9121c30b5cd02f9fb3ce5b269ba9024f19e4dc7
Gabriel Pezzo <gabriel.pezzo@gbmlogistica.com.br>
2024-04-18 16:07:23 -0300

    feat: Localized Script for TextMeshPro

---
+++ Assets/Scripts/Localization/SimpleLocalization/Scripts/LocalizedTextMeshPro.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.SimpleLocalization.Scripts
+{
+	/// <summary>
+	/// Localize text component.
+	/// </summary>
+    [RequireComponent(typeof(TextMeshPro))]
+    public class LocalizedTextMeshPro : MonoBehaviour
+    {
+        public string LocalizationKey;
+
+        public void Start()
+        {
+            Localize();
+            LocalizationManager.OnLocalizationChanged += Localize;
+        }
+
+        public void OnDestroy()
+        {
+            LocalizationManager.OnLocalizationChanged -= Localize;
+        }
+
+        private void Localize()
+        {
+            GetComponent<TextMeshPro>().text = LocalizationManager.Localize(LocalizationKey);
+        }
+    }
+}
