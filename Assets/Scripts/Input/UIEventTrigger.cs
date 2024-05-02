6f0db96d3db47885e2558a85a3a96a1cdaae6b25
Gabriel Pezzo <gabriel.pezzo@gbmlogistica.com.br>
2024-05-02 19:10:55 -0300

    refac: UIEventTrigger

--- Assets/Scripts/Input/UIEventTrigger.cs
+++ Assets/Scripts/Input/UIEventTrigger.cs
@@ -1,34 +1,97 @@
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using static CursorManager;
 
+[RequireComponent(typeof(Selectable))]
 public class UIEventTrigger : EventTrigger
 {
-    Animator FadeController;
+    Animator fadeController;
+    Selectable selectable;
+    public bool isSelected;
 
     void Start() {
-        TryGetComponent(out FadeController);
+        TryGetComponent(out fadeController);
+        selectable = GetComponent<Selectable>();
+
+
+        if(isSelected) 
+            this.FadeIn();
     }
+
     public override void OnPointerEnter(PointerEventData data) {
-        FadeController?.Play("Fade_In");
+        if(!selectable.interactable) 
+            return;
+        fadeController?.Play("Fade_In");
         SetCursor(KingdomCursor.Hover);
     }
     public override void OnPointerExit(PointerEventData data){
-        FadeController?.Play("Fade_Out");
+        if(!selectable.interactable) 
+            return;
+        if(!isSelected) 
+            FadeOut();
+
         SetCursor(KingdomCursor.Default);
     }     
 
     public override void OnPointerDown(PointerEventData data){
+        if(!selectable.interactable) 
+            return;
+
         SetCursor(KingdomCursor.Click);
     }
 
     public override void OnPointerUp(PointerEventData data)
     {
+        if(!selectable.interactable) 
+            return;
+
         if(GetCursor() is KingdomCursor.Click)
             SetCursor(KingdomCursor.Hover);
     }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button != PointerEventData.InputButton.Left && !selectable.interactable) 
+            return;
+
+        selectable.Select();
+        this.OnSubmit(eventData);
+        base.OnPointerClick(eventData);
+    }
+
     public void OnDisable()
     {
         SetCursor(KingdomCursor.Default);
     }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        if(!selectable.interactable) 
+            return;
+
+        this.FadeOut();
+        isSelected = false;
+    }
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        if(!selectable.interactable) 
+            return;
+
+        this.FadeIn();
+        isSelected = true;
+    }
+
+    public override void OnSubmit(BaseEventData eventData)
+    {
+        if(!selectable.interactable) 
+            return;
+
+        base.OnSubmit(eventData);
+    }
+
+    public void FadeOut() => fadeController?.Play("Fade_Out");
+    public void FadeIn() => fadeController?.Play("Fade_In");
 }
