9de2e3dc219a8a9b8282c61b50b60e981e7db520
Gabriel Pezzo <gabriel.pezzo@gbmlogistica.com.br>
2024-05-07 18:40:42 -0300

    wip: audio settings

---
+++ Assets/Scripts/Managers/Settings/SettingsManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    private bool fullscreen = true;
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        foreach (var setting in Screen.resolutions)
+            SetResolution(setting.ToString());
+    }
+    public IEnumerable<Resolution> GetSupportedResolutions() => Screen.resolutions;
+
+    public void SetResolution(string resolution)
+    {
+        string[] parts = resolution.Split(new string[] { "x", "@", "Hz" }, StringSplitOptions.RemoveEmptyEntries);
+        int width = int.Parse(parts[0].Trim());
+        int height = int.Parse(parts[1].Trim());
+        int refreshRate = (int)Math.Ceiling(double.Parse(parts[2].Trim()));
+        Screen.SetResolution(width, height, fullscreen);
+    }
+}
--- Assets/Scripts/Systems/Audio/AudioSystem.cs
+++ Assets/Scripts/Systems/Audio/AudioSystem.cs
@@ -14,11 +14,13 @@ public class AudioSystem : PersistentSingleton<AudioSystem>
     [SerializeField]
     private ScriptableContainer audioContainer;
     private AudioSource audioSource;
+    [Range(0,1)]
+    private float globalVolume;
     protected override void Awake()
     {
         base.Awake();
         audioSource = GetComponent<AudioSource>();
-
+        globalVolume = 1.0f;
     }
 
     void OnEnable() {
@@ -34,7 +36,7 @@ public class AudioSystem : PersistentSingleton<AudioSystem>
     public void Play(UIAction action) 
  { 
     var audio = audioContainer.GetByType<UIAudio>().First(a => a.Action == action);
-    audioSource.PlayOneShot(audio.AudioClip, audio.volume);
+    audioSource.PlayOneShot(audio.AudioClip, audio.volume * globalVolume);
  }
     
     public void Play(Scene stage)
@@ -42,12 +44,19 @@ public class AudioSystem : PersistentSingleton<AudioSystem>
         audioSource.Stop();
         var audio = audioContainer.GetByType<StageAudio>().First(a => a.StageName == stage.name);
             audioSource.clip = audio.AudioClip;	
-            audioSource.volume = audio.volume;
+            audioSource.volume *= audio.volume;
         audioSource.Play();
     }
 
     public void Play(EnemyID enemy, ActorAudioTypes actorAudioType) => audioSource.PlayOneShot(audioContainer.GetFirstByType<EnemyAudio>(so => so.Enemy == enemy && so.AudioType == actorAudioType).AudioClip);
     public void Play(CharacterID player, ActorAudioTypes actorAudioType) => audioSource.PlayOneShot(audioContainer.GetFirstByType<PlayerAudio>(so => so.Player == player && so.AudioType == actorAudioType).AudioClip);
+
+    public void SetVolume(float volume)
+    {
+        this.audioSource.volume /= globalVolume;
+        this.globalVolume = volume;
+        this.audioSource.volume *= volume;
+    }
 }
 
 
