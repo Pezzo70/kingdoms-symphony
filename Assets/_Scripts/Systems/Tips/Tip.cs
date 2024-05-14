d1b1372f5713a3e3ac120da22013d0e85d199e9d
Rezende <rezendedev@gmail.com>
2024-05-14 12:03:05 -0300

    fix: tip scriptable object

--- Assets/_Scripts/Systems/Tips/Tip.cs
+++ Assets/_Scripts/Systems/Tips/Tip.cs
@@ -1,6 +1,5 @@
 using System;
 using Kingdom.Enemies;
-using UnityEditor;
 using UnityEngine;
 
 namespace Kingdom.Enums.Tips
@@ -11,21 +10,5 @@ namespace Kingdom.Enums.Tips
         public TipID tipID;
         public bool isEnemy;
         public EnemyID enemyID;
-    }
-
-    [CustomEditor(typeof(Tip))]
-    public class TipEditor : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            var tip = target as Tip;
-
-            tip.tipID = (TipID)EditorGUILayout.EnumPopup("Tip ID:", tip.tipID);
-
-            tip.isEnemy = GUILayout.Toggle(tip.isEnemy, "Is Enemy");
-
-            if (tip.isEnemy)
-                tip.enemyID = (EnemyID)EditorGUILayout.EnumPopup("Enemy ID:", tip.enemyID);
-        }
     }
 }
