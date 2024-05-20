df27c72222a2b042771f11d8e99a9e417c2dc016
Rezende <rezendedev@gmail.com>
2024-05-19 22:11:10 -0300

    feat: enemies spawn

---
+++ Assets/_Scripts/Systems/Enemies/EnemyEntity.cs
@@ -0,0 +1,14 @@
+using Kingdom.Enemies;
+using UnityEngine;
+
+namespace Kingdom.Enemies
+{
+    public class EnemyEntity : MonoBehaviour
+    {
+        public Enemy enemyData;
+
+        void Start() { }
+
+        void Update() { }
+    }
+}
